8a06b3da746168b9427139b8f946b352a466377f
xavibona <xavierbonachera@outlook.com>
2024-07-29 13:10:07 -0400

    comienzo de 19

---
+++ clase19/script.js
@@ -0,0 +1,8 @@
+/* console.dir(document) // document es una variable global que nos muestra el html como objeto de js
+ */
+
+// getElementById() => permite tener un elemento x su id (null si no hay nada)
+const titulo = document.getElementById('titulo')
+const subtitulo = document.getElementById('subtitulo')
+console.log(subtitulo)
+console.log(titulo)
